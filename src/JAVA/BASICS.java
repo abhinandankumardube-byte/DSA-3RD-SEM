d0535667b83a0314c107a5414f8b5128e886a565
abhinandankumardube-byte <abhinandankumardube@gmail.com>
2026-07-30 11:42:11 +0530

    Scanner class

---
+++ src/JAVA/BASICS.java
@@ -0,0 +1,13 @@
+package JAVA;
+
+import java.util.Scanner;
+
+public class BASICS {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        float b = sc.nextFloat();
+        float sum = a + b;
+        System.out.println(sum);
+    }
+}
